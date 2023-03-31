1999b680a6a1b62eda5f4ba06f450b8673c62dfb
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-31 02:55:52 +0200

    fine esercizio

--- Program.cs
+++ Program.cs
@@ -5,19 +5,33 @@ using System.Data.SqlClient;
 var connStr = "Data Source = localhost;Initial Catalog=db-videogames;Integrated Security=True;Encrypt=False;";
 var vid = new videogamemanager(connStr);
 
-Console.WriteLine("inserisci un nome");
+//Aggiunta videogame
+
+/*Console.WriteLine("inserisci un nome");
 var name =Console.ReadLine();
 Console.WriteLine("inserisci una descrizione");
 var ow = Console.ReadLine();
-var getadd = vid.addvideogame(name,ow);
+Console.WriteLine("inserisci una data");
+var date =Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("inserisci l'id della softwarehouse");
+var softwarehouse = Convert.ToInt64(Console.ReadLine());
+vid.addvideogame(name,ow,date,softwarehouse);*/
 
 
-Console.WriteLine("inserisci l'id da ricercare");
+//Ricerca videogame
+
+/*Console.WriteLine("inserisci l'id da ricercare");
 var dato1=Convert.ToInt64(Console.ReadLine());
 Console.WriteLine("inserisci il nome da ricercare");
 var dato2 = Console.ReadLine();
 var getinput = vid.getvideogame(dato1,dato2);
-foreach (var game in getinput) Console.WriteLine(game);
+foreach (var game in getinput) Console.WriteLine(game);*/
 
 
+
+//Rimozione videogame
+
+Console.WriteLine("Inserisci il nome del videogame da rimuovere dalla lista");
+string remove = Console.ReadLine();
+vid.removegame(remove);
 
--- videogamemanager.cs
+++ videogamemanager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,49 +54,69 @@ namespace adonet_db_videogame
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }finally { 
+                conn.Close();
+                Environment.Exit(0);
             }
             return gamelist;
         }
 
-        public List<Videogame> addvideogame(string names, string overviews)
+        public void addvideogame(string Name, string ows, DateTime release, long softwarehouse)
         {
-            var conn = new SqlConnection(connStr);
-            var gamelist = new List<Videogame>();
+            var connectionString = "Data Source = localhost;Initial Catalog=db-videogames;Integrated Security=True;Encrypt=False";
+            string query = "INSERT INTO videogames (name,overview,release_date,software_house_id) VALUES(@Name,@ows,@release_date,@software_house_id)";
 
-            try
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                conn.Open();
-                var query = "INSERT INTO videogames(name, overview)"
-                    + "VALUES(@NameLike,@overlike)";
-
-
-                using var command = new SqlCommand(query, conn);
-                command.Parameters.AddWithValue("@NameLike", $"{names}");
-                command.Parameters.AddWithValue("@overlike", $"{overviews}");
-
-                using SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
+                try
                 {
-                    var ididx = reader.GetOrdinal("id");
-                    var id = reader.GetInt64(ididx);
 
-                    var nameidx = reader.GetOrdinal("name");
-                    var name = reader.GetString(nameidx);
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.Add(new SqlParameter("@Name", $"{Name}"));
+                cmd.Parameters.Add(new SqlParameter("@ows", $"{ows}"));
+                cmd.Parameters.Add(new SqlParameter("@release_date", release));
+                cmd.Parameters.Add(new SqlParameter("@software_house_id", softwarehouse));
 
-                    var vvidx = reader.GetOrdinal("overview");
-                    var overview = reader.GetString(vvidx);
-
-                    var game = new Videogame(id, name, overview);
-                    gamelist.Add(game);
+                connection.Open();
+                int affectedRows = cmd.ExecuteNonQuery();
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally {
+                    connection.Close();
+                    Environment.Exit(0);
                 }
 
             }
-            catch
+        }
+
+        public void removegame(string Name)
+        {
+            var connectionString = "Data Source = localhost;Initial Catalog=db-videogames;Integrated Security=True;Encrypt=False";
+            string query = "DELETE FROM videogames WHERE name = @name";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                throw new Exception("errore");
+                try
+                {
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.Add(new SqlParameter("@name", Name));
+                connection.Open();
+                int affectedRows = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }finally {
+                    connection.Close();
+                    Environment.Exit(0);
+                }
+
             }
-            return gamelist;
+
+            
         }
     }
 }
